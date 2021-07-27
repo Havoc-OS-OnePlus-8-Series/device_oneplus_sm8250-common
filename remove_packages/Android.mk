7e3758af8aef38ad7259983dbd59cef8967e92b3
chandu078 <chandudyavanapelli03@gmail.com>
2021-07-27 20:16:04 +0530

    sm8250-common: proprietary-files: Update Dolby blobs * From OnePlus9ROxygen_13.I.04_OTA_0040_all_2105290149_3f45a99b8c1a43eb * Also remove AudioFX
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Camera2 GalleryGoPrebuilt Gallery2 MusicFX NfcNci Photos RecorderPrebuilt SafetyHubPrebuilt SoundAmplifierPrebuilt Snap
+LOCAL_OVERRIDES_PACKAGES := AudioFX Camera2 GalleryGoPrebuilt Gallery2 MusicFX NfcNci Photos RecorderPrebuilt SafetyHubPrebuilt SoundAmplifierPrebuilt Snap
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
