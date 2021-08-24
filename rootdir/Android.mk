ace39a7e9597612bd9da2f61ebf900bbab314d4c
chandu078 <chandudyavanapelli03@gmail.com>
2021-08-24 22:02:25 +0530

    sm8250-common: Merge kebab device tree into common tree * Device configs * Device overlay * Device rootdir * Device blobs * Device vendor properties
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE           := fstab.qcom
+LOCAL_MODULE_TAGS      := optional
+LOCAL_MODULE_CLASS     := ETC
+LOCAL_SRC_FILES        := etc/fstab.qcom
+LOCAL_REQUIRED_MODULES := fstab.qcom.ramdisk
+LOCAL_MODULE_PATH      := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE           := fstab.qcom.ramdisk
+LOCAL_MODULE_STEM      := fstab.qcom
+LOCAL_MODULE_TAGS      := optional
+LOCAL_MODULE_CLASS     := ETC
+LOCAL_SRC_FILES        := etc/fstab.qcom
+LOCAL_MODULE_PATH      := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.class_main.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
